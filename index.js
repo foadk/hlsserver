fb6a803a01838902339cf793e05e388c00273972
Foad <khaleghi.foad@gmail.com>
2019-01-02 22:29:00 +0330

    initiate hls

--- index.js
+++ index.js
@@ -4,6 +4,38 @@ var fs = require('fs');
 const app = express()
 const port = 3000
 
+app.get('/hls/:token/playlist', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    fs.stat('example_m3u8/playlist.m3u8', function (err, stats) {
+        if (!stats || err) {
+            res.status(404).end()
+        }
+        else {
+            // res.status(200).set('Content-Type', 'application/vnd.apple.mpegurl');
+            // fs.createReadStream('example_m3u8/playlist.m3u8').pipe(res)
+            fs.createReadStream('example_m3u8/playlist.m3u8').pipe(res)
+        }
+
+    })
+})
+
+app.get('/hls/:token/:name', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    fs.stat('example_m3u8/' + req.params.name, function (err, stats) {
+        if (!stats || err) {
+            res.status(404).end()
+        }
+        else {
+            // res.status(200).set('Content-Type', 'application/vnd.apple.mpegurl');
+            // fs.createReadStream('example_m3u8/playlist.m3u8').pipe(res)
+            fs.createReadStream('example_m3u8/' + req.params.name).pipe(res)
+        }
+
+    })
+})
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
