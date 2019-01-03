317cc713692d0d39a6e82f241b29546cdedae51c
Foad <khaleghi.foad@gmail.com>
2019-01-03 10:25:27 +0330

    Authorize requests with JWT

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
 const express = require('express')
 var fs = require('fs');
+require('dotenv').config({ path: '../hls/.env' })
+var jwt = require('jsonwebtoken');
 
 const app = express()
 const port = 3000
 
-app.get('/hls/:token/playlist', (req, res) => {
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use('/hls/:token', (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    try {
+        jwt.verify(req.params.token, process.env.JWT_SECRET)
+    } catch(err) {
+        res.status(401).end()
+    }
+    next()
+})
+
+app.get('/hls/:token/playlist', (req, res) => {
     fs.stat('example_m3u8/playlist.m3u8', function (err, stats) {
         if (!stats || err) {
             res.status(404).end()
-        }
-        else {
-            // res.status(200).set('Content-Type', 'application/vnd.apple.mpegurl');
-            // fs.createReadStream('example_m3u8/playlist.m3u8').pipe(res)
+        } else {
             fs.createReadStream('example_m3u8/playlist.m3u8').pipe(res)
         }
-
     })
 })
 
 app.get('/hls/:token/:name', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     fs.stat('example_m3u8/' + req.params.name, function (err, stats) {
         if (!stats || err) {
             res.status(404).end()
-        }
-        else {
-            // res.status(200).set('Content-Type', 'application/vnd.apple.mpegurl');
-            // fs.createReadStream('example_m3u8/playlist.m3u8').pipe(res)
+        } else {
             fs.createReadStream('example_m3u8/' + req.params.name).pipe(res)
         }
-
     })
 })
-
-app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
